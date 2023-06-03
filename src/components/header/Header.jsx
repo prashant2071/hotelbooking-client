23d225614f7ebcec83a51cf6761ed8a2b7a84016
prashanta <pbt9840@gmail.com>
2023-06-03 19:47:38 +0545

    date Range added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,39 @@
-import "./header.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faBinoculars, faCalendar, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import "./header.css";
+import { DateRangePicker, DateRange } from "react-date-range";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faBinoculars,
+  faCalendar,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
   return (
-    <div className='header'>
+    <div className="header">
       <div className="headerContainer">
         <div className="headerList">
-            <div className="headerListItem active">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
-            </div>
-            <div className="headerListItem">
+          </div>
+          <div className="headerListItem">
             <FontAwesomeIcon icon={faPlane} />
             <span>Flights</span>
           </div>
@@ -29,34 +52,47 @@ const Header = () => {
         </div>
         <h1 className="headerTitle">A lifetime of discount?</h1>
         <p className="headerDescription">
-          Get rewarded on the travel - unlock the instant saving of 10% or
-          more with hotel booking  account
+          Get rewarded on the travel - unlock the instant saving of 10% or more
+          with hotel booking account
         </p>
         <button className="headerBtn">Sign in / Register</button>
         <div className="headerSearch">
           <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faBed} className="headerIcon"/>
-            <input 
-            type="text"
-            placeholder="where are you going"
-            className="headerSearchInput"
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="where are you going"
+              className="headerSearchInput"
             />
           </div>
           <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
-            <span className="headerSearchText">date to date</span>
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span onClick={()=>setOpenDate(!openDate)} className="headerSearchText">{`${format(
+              date[0].startDate,
+              "MM/dd/yyyy"
+            )} to 
+            ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+            {openDate && 
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+              />
+            }
           </div>
           <div className="headerSearchItem">
-            <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
             <span className="headerSearchText">2 adult 2 children 1 room</span>
           </div>
           <div className="headerSearchItem">
-          <button className="headerBtn">Search</button>
+            <button className="headerBtn">Search</button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
