fc3f86faa87a3d89e7c0bde334a7c2c6ccb25850
prashanta <pbt9840@gmail.com>
2023-06-10 09:43:56 +0545

    footer complete and list wrapper started

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="fLists">
+        <ul className="flist">
+          <li className='fListItem'>countries</li>
+          <li className='fListItem'>Region</li>
+          <li className='fListItem'>Cities</li>
+          <li className='fListItem'>District</li>
+          <li className='fListItem'>Airports</li>
+          <li className='fListItem'>Hotels</li>
+        </ul>
+        <ul className="flist">
+          <li className='fListItem'>countries</li>
+          <li className='fListItem'>Region</li>
+          <li className='fListItem'>Cities</li>
+          <li className='fListItem'>District</li>
+          <li className='fListItem'>Airports</li>
+          <li className='fListItem'>Hotels</li>
+        </ul>        <ul className="flist">
+          <li className='fListItem'>countries</li>
+          <li className='fListItem'>Region</li>
+          <li className='fListItem'>Cities</li>
+          <li className='fListItem'>District</li>
+          <li className='fListItem'>Airports</li>
+          <li className='fListItem'>Hotels</li>
+        </ul>        <ul className="flist">
+          <li className='fListItem'>countries</li>
+          <li className='fListItem'>Region</li>
+          <li className='fListItem'>Cities</li>
+          <li className='fListItem'>District</li>
+          <li className='fListItem'>Airports</li>
+          <li className='fListItem'>Hotels</li>
+        </ul>        <ul className="flist">
+          <li className='fListItem'>countries</li>
+          <li className='fListItem'>Region</li>
+          <li className='fListItem'>Cities</li>
+          <li className='fListItem'>District</li>
+          <li className='fListItem'>Airports</li>
+          <li className='fListItem'>Hotels</li>
+        </ul>
+      </div>
+      <div className="fText">Copyright © 1996–2023  Booking.com™. All rights reserved.
+ </div>
+    </div>
   )
 }
 
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -7,6 +7,15 @@ const List = () => {
     <div>
       <Navbar/>
       <Header type="list"/>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+          </div>
+          <div className="listReasult">
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
