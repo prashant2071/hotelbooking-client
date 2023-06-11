c8fe29956205237ce9231175acf5d2648acb3554
prashanta <pbt9840@gmail.com>
2023-06-11 19:16:00 +0545

    page desc

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -2,12 +2,28 @@ import './searchItem.css'
 
 const SearchItem = () => {
   return (
-    <div className='SearchItem'>
-        <img src="" alt="" className="siImg" />
-        <div className="siDesc">
-        
-        </div>
-        <div className="siDetails"></div>
+    <div className="searchItem">
+    <img
+      src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
+      alt=""
+      className="siImg"
+    />
+    <div className="siDesc">
+      <h1 className="siTitle">Tower Street Apartments</h1>
+      <span className="siDistance">500m from center</span>
+      <span className="siTaxiOp">Free airport taxi</span>
+      <span className="siSubtitle">
+        Studio Apartment with Air conditioning
+      </span>
+      <span className="siFeatures">
+        Entire studio • 1 bathroom • 21m² 1 full bed
+      </span>
+      <span className="siCancelOp">Free cancellation </span>
+      <span className="siCancelOpSubtitle">
+        You can cancel later, so lock in this great price today!
+      </span>
+    </div>
+    <div className='siDetails'>details</div>
     </div>
   )
 }
