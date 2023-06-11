786413244dbb1a14acb265ec404f29b9aa84255e
prashanta <pbt9840@gmail.com>
2023-06-11 06:13:42 +0545

    search box

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -37,6 +37,41 @@ const List = () => {
                 />
               )}
             </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOptions">
+              <div className="lsOptionItem">
+              <span className="lsOptionsText">
+                Min Price <small>per night</small>
+              </span>
+              <input type='number' className='lsOptionInput'/>
+              </div>
+              <div className="lsOptionItem">
+              <span className="lsOptionsText">
+                Max Price <small>per night</small>
+              </span>
+              <input type='number' className='lsOptionInput'/>
+              </div><div className="lsOptionItem">
+              <span className="lsOptionsText">
+                Adult
+              </span>
+              <input type='number' min={1} className='lsOptionInput' placeholder={options
+              .adult}/>
+              </div><div className="lsOptionItem">
+              <span className="lsOptionsText">
+                Children
+              </span>
+              <input type='number' min={0} className='lsOptionInput' placeholder={options.children}/>
+              </div>
+              <div className="lsOptionItem">
+              <span className="lsOptionsText">
+                Room
+              </span>
+              <input type='number' min={1} className='lsOptionInput' placeholder={options.room}/>
+              </div>
+            </div>
+          </div>
+            <button >Search</button>
           </div>
           <div className="listResult">
 
